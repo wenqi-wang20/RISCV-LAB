340849742373be083481874d04457e6496584bd9
gaoyichuan <gaoyichuan000@gmail.com>
2022-09-12 16:37:22 +0800

    Initial template for 2022 labs

---
+++ thinpad_top.srcs/sources_1/new/lab5/lab5_master.sv
@@ -0,0 +1,23 @@
+module lab5_master #(
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 32
+) (
+    input wire clk_i,
+    input wire rst_i,
+
+    // TODO: 添加需要的控制信号，例如按键开关？
+
+    // wishbone master
+    output reg wb_cyc_o,
+    output reg wb_stb_o,
+    input wire wb_ack_i,
+    output reg [ADDR_WIDTH-1:0] wb_adr_o,
+    output reg [DATA_WIDTH-1:0] wb_dat_o,
+    input wire [DATA_WIDTH-1:0] wb_dat_i,
+    output reg [DATA_WIDTH/8-1:0] wb_sel_o,
+    output reg wb_we_o
+);
+
+  // TODO: 实现实验 5 的内存+串口 Master
+
+endmodule
