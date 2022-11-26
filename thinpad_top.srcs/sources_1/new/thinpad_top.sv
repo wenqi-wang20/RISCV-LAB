f64c4fb72410bd059563a4ff720a7f30443db114
vextawang <vextawang@gmail.com>
2022-11-26 14:36:38 +0800

    add vga

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -660,4 +660,22 @@ module thinpad_top (
 
   /* =========== Wishbone Slaves end =========== */
 
+  /* =========== VGA begin =========== */
+  // 图像输出演示，分辨率 800x600@75Hz，像素时钟为 50MHz
+  logic [11:0] hdata;
+  assign video_red   = hdata < 266 ? 3'b111 : 0;  // 红色竖条
+  assign video_green = hdata < 532 && hdata >= 266 ? 3'b111 : 0;  // 绿色竖条
+  assign video_blue  = hdata >= 532 ? 2'b11 : 0;  // 蓝色竖条
+  assign video_clk   = clk_50M;
+  vga #(12, 800, 856, 976, 1040, 600, 637, 643, 666, 1, 1) vga800x600at75 (
+      .clk        (clk_50M),
+      .hdata      (hdata),        // 横坐标
+      .vdata      (),             // 纵坐标
+      .hsync      (video_hsync),
+      .vsync      (video_vsync),
+      .data_enable(video_de)
+  );
+  /* =========== VGA end =========== */
+  
+
 endmodule
